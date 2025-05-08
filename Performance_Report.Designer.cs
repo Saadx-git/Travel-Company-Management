89c3b359063995d44a85b518e8b32425f0f72a49
MuhammadHasan-04 <153608670+MuhammadHasan-04@users.noreply.github.com>
2025-05-08 21:38:28 +0500

    Update Performance_Report.Designer.cs

--- Performance_Report.Designer.cs
+++ Performance_Report.Designer.cs
@@ -1,5 +1,4 @@
-﻿using System.Drawing;
-using System.Drawing.Drawing2D;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace @new
@@ -7,6 +6,7 @@ namespace @new
     partial class Form6
     {
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.DataGridView dataGridViewPerformance;
 
         protected override void Dispose(bool disposing)
         {
@@ -18,19 +18,9 @@ namespace @new
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.lblOccupancy = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.lblRating = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.btnView = new System.Windows.Forms.Button();
-            this.label6 = new System.Windows.Forms.Label();
             this.btnBack = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
-            this.panel3.SuspendLayout();
+            this.dataGridViewPerformance = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPerformance)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -43,107 +33,6 @@ namespace @new
             this.label1.TabIndex = 0;
             this.label1.Text = "Provider Performance Report";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(230)))), ((int)(((byte)(250)))));
-            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Controls.Add(this.lblOccupancy);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Location = new System.Drawing.Point(180, 150);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(280, 200);
-            this.panel1.TabIndex = 1;
-            // 
-            // lblOccupancy
-            // 
-            this.lblOccupancy.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold);
-            this.lblOccupancy.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(80)))), ((int)(((byte)(200)))));
-            this.lblOccupancy.Location = new System.Drawing.Point(60, 80);
-            this.lblOccupancy.Name = "lblOccupancy";
-            this.lblOccupancy.Size = new System.Drawing.Size(160, 65);
-            this.lblOccupancy.TabIndex = 0;
-            this.lblOccupancy.Text = "76%";
-            this.lblOccupancy.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label2
-            // 
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
-            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(65)))), ((int)(((byte)(165)))));
-            this.label2.Location = new System.Drawing.Point(60, 30);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(160, 30);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Occupancy";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(230)))), ((int)(((byte)(250)))));
-            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel2.Controls.Add(this.lblRating);
-            this.panel2.Controls.Add(this.label4);
-            this.panel2.Location = new System.Drawing.Point(500, 150);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(280, 200);
-            this.panel2.TabIndex = 2;
-            // 
-            // lblRating
-            // 
-            this.lblRating.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold);
-            this.lblRating.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(80)))), ((int)(((byte)(200)))));
-            this.lblRating.Location = new System.Drawing.Point(50, 80);
-            this.lblRating.Name = "lblRating";
-            this.lblRating.Size = new System.Drawing.Size(180, 65);
-            this.lblRating.TabIndex = 0;
-            this.lblRating.Text = "4.3★";
-            this.lblRating.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label4
-            // 
-            this.label4.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
-            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(65)))), ((int)(((byte)(165)))));
-            this.label4.Location = new System.Drawing.Point(90, 30);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(100, 30);
-            this.label4.TabIndex = 1;
-            this.label4.Text = "Rating";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(230)))), ((int)(((byte)(250)))));
-            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel3.Controls.Add(this.btnView);
-            this.panel3.Controls.Add(this.label6);
-            this.panel3.Location = new System.Drawing.Point(820, 150);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(280, 200);
-            this.panel3.TabIndex = 3;
-            // 
-            // btnView
-            // 
-            this.btnView.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(80)))), ((int)(((byte)(200)))));
-            this.btnView.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnView.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
-            this.btnView.ForeColor = System.Drawing.Color.White;
-            this.btnView.Location = new System.Drawing.Point(85, 98);
-            this.btnView.Name = "btnView";
-            this.btnView.Size = new System.Drawing.Size(115, 54);
-            this.btnView.TabIndex = 0;
-            this.btnView.Text = "View";
-            this.btnView.UseVisualStyleBackColor = false;
-            // 
-            // label6
-            // 
-            this.label6.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
-            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(65)))), ((int)(((byte)(165)))));
-            this.label6.Location = new System.Drawing.Point(80, 30);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(120, 30);
-            this.label6.TabIndex = 2;
-            this.label6.Text = "Feedback";
-            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // btnBack
             // 
@@ -160,6 +49,17 @@ namespace @new
             this.btnBack.UseVisualStyleBackColor = false;
             this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
+            // dataGridViewPerformance
+            // 
+            this.dataGridViewPerformance.AllowUserToAddRows = false;
+            this.dataGridViewPerformance.AllowUserToDeleteRows = false;
+            this.dataGridViewPerformance.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewPerformance.Location = new System.Drawing.Point(40, 129);
+            this.dataGridViewPerformance.Name = "dataGridViewPerformance";
+            this.dataGridViewPerformance.ReadOnly = true;
+            this.dataGridViewPerformance.Size = new System.Drawing.Size(1200, 200);
+            this.dataGridViewPerformance.TabIndex = 25;
+            // 
             // Form6
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -168,31 +68,16 @@ namespace @new
             this.ClientSize = new System.Drawing.Size(1280, 720);
             this.Controls.Add(this.btnBack);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.dataGridViewPerformance);
             this.Name = "Form6";
             this.Text = "Provider Performance Report";
-            this.Load += new System.EventHandler(this.Form6_Load_1);
-            this.panel1.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
-            this.panel3.ResumeLayout(false);
+            this.Load += new System.EventHandler(this.Form6_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPerformance)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         private Label label1;
-        private Panel panel1;
-        private Label lblOccupancy;
-        private Label label2;
-        private Panel panel2;
-        private Label lblRating;
-        private Label label4;
-        private Panel panel3;
-        private Button btnView;
-        private Label label6;
-
-
         private Button btnBack;
     }
 }
